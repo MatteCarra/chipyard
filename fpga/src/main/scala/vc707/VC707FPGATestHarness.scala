a728b8f91f31463d2fc56ec6b5f4573f7571de70
MatteCarra <matte.carra@gmail.com>
2022-05-24 17:38:57 +0200

    VC707: do not place an additional sys_clock

--- fpga/src/main/scala/vc707/VC707FPGATestHarness.scala
+++ fpga/src/main/scala/vc707/VC707FPGATestHarness.scala
@@ -1,12 +1,13 @@
 package chipyard.fpga.vc707
 
 import chipyard.{CanHaveMasterTLMemPort, ChipTop, DefaultClockFrequencyKey, ExtTLMem}
+import chisel3.Clock
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy.{BundleBridgeSource, LazyModule}
 import freechips.rocketchip.tilelink.TLClientNode
 import sifive.blocks.devices.uart.{PeripheryUARTKey, UARTPortIO}
 import sifive.fpgashells.clocks.{ClockGroup, ClockSinkNode, PLLFactoryKey, ResetWrangler}
-import sifive.fpgashells.shell.xilinx.VC707BaseShell
+import sifive.fpgashells.shell.xilinx.{SysClockVC707PlacedOverlay, VC707BaseShell}
 import sifive.fpgashells.shell.{ClockInputDesignInput, ClockInputOverlayKey, DDRDesignInput, DDROverlayKey, UARTDesignInput, UARTOverlayKey}
 
 class VC707FPGATestHarness(override implicit val p: Parameters) extends VC707BaseShell {
@@ -15,7 +16,10 @@ class VC707FPGATestHarness(override implicit val p: Parameters) extends VC707Bas
   // DOC include start: ClockOverlay
   // place all clocks in the shell
   require(dp(ClockInputOverlayKey).size >= 1)
-  val sysClkNode = dp(ClockInputOverlayKey)(0).place(ClockInputDesignInput()).overlayOutput.node
+
+  val sysClkNode = sys_clock.get() match {
+    case Some(x: SysClockVC707PlacedOverlay) => x.node
+  }
 
   /*** Connect/Generate clocks ***/
 
@@ -24,7 +28,7 @@ class VC707FPGATestHarness(override implicit val p: Parameters) extends VC707Bas
   harnessSysPLL := sysClkNode
 
   // create and connect to the dutClock
-  println(s"VCU118 FPGA Base Clock Freq: ${dp(DefaultClockFrequencyKey)} MHz")
+  println(s"VC707 FPGA Base Clock Freq: ${dp(DefaultClockFrequencyKey)} MHz")
   val dutClock = ClockSinkNode(freqMHz = dp(DefaultClockFrequencyKey))
   val dutWrangler = LazyModule(new ResetWrangler)
   val dutGroup = ClockGroup()
@@ -49,7 +53,4 @@ class VC707FPGATestHarness(override implicit val p: Parameters) extends VC707Bas
   }
   val ddrClient = TLClientNode(Seq(inParams.master))
   ddrNode := ddrClient
-
-  /*** DDR ***/
-
 }
