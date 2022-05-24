12705567c5da93d709e6ff1eb153424733deba0f
MatteCarra <matte.carra@gmail.com>
2022-05-24 18:52:07 +0200

    VC707: reworked testharness

--- fpga/src/main/scala/vc707/HarnessBinders.scala
+++ fpga/src/main/scala/vc707/HarnessBinders.scala
@@ -1,32 +1,43 @@
 package chipyard.fpga.vc707
 
-import chipyard.{CanHaveMasterTLMemPort}
+import chipyard.{CanHaveMasterTLMemPort, HasHarnessSignalReferences}
 import chipyard.harness.OverrideHarnessBinder
 import chisel3.Wire
+import chisel3.experimental.BaseModule
 import freechips.rocketchip.tilelink.TLBundle
 import freechips.rocketchip.util.HeterogeneousBag
 import sifive.blocks.devices.spi.{HasPeripherySPI, SPIPortIO}
 import sifive.blocks.devices.uart.{HasPeripheryUARTModuleImp, UARTPortIO}
 
+/*** UART ***/
 class WithUART extends OverrideHarnessBinder({
-  (system: HasPeripheryUARTModuleImp, th: VC707FPGATestHarness, ports: Seq[UARTPortIO]) => {
-    th.io_uart_bb.bundle <> ports.head
+  (system: HasPeripheryUARTModuleImp, th: BaseModule with HasHarnessSignalReferences, ports: Seq[UARTPortIO]) => {
+    th match { case vc707th: VC707FPGATestHarnessImp => {
+      vc707th.vc707Outer.io_uart_bb.bundle <> ports.head
+    } }
   }
 })
 
+/*** SPI ***/
 class WithSPISDCard extends OverrideHarnessBinder({
-  (system: HasPeripherySPI, th: VC707FPGATestHarness, ports: Seq[SPIPortIO]) => {
-    th.io_spi_bb.bundle <> ports.head
+  (system: HasPeripherySPI, th: BaseModule with HasHarnessSignalReferences, ports: Seq[SPIPortIO]) => {
+    th match { case vc707th: VC707FPGATestHarnessImp => {
+      vc707th.vc707Outer.io_spi_bb.bundle <> ports.head
+    } }
   }
 })
 
+/*** Experimental DDR ***/
 class WithDDRMem extends OverrideHarnessBinder({
-  (system: CanHaveMasterTLMemPort, th: VC707FPGATestHarness, ports: Seq[HeterogeneousBag[TLBundle]]) => {
-    require(ports.size == 1)
+  (system: CanHaveMasterTLMemPort, th: BaseModule with HasHarnessSignalReferences, ports: Seq[HeterogeneousBag[TLBundle]]) => {
+    th match { case vc707th: VC707FPGATestHarnessImp => {
+      require(ports.size == 1)
 
-    val bundles = th.ddrClient.out.map(_._1)
-    val ddrClientBundle = Wire(new HeterogeneousBag(bundles.map(_.cloneType)))
-    bundles.zip(ddrClientBundle).foreach { case (bundle, io) => bundle <> io }
-    ddrClientBundle <> ports.head
+      val bundles = vc707th.vc707Outer.ddrClient.out.map(_._1)
+      val ddrClientBundle = Wire(new HeterogeneousBag(bundles.map(_.cloneType)))
+      bundles.zip(ddrClientBundle).foreach { case (bundle, io) => bundle <> io }
+      ddrClientBundle <> ports.head
+    } }
   }
 })
+
